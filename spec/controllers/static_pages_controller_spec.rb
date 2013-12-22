50acd01afc4ae91e9b498a0c43a5bdee29a8d235
Matthew Star <mattjstar@gmail.com>
2013-12-22 13:00:26 -0500

    static_pages_controller_spec has green test suite

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+	describe "#home" do
+		describe "true_waist_search" do
+			before do
+				@small_jean = create(:item, waist: 32)
+				@large_jean = create(:item, waist: 33)
+			end
+
+			it "should only return locations with microposts" do
+				get :home, { :waist => 32 }
+				assigns(:jeans).map(&:id).should == [@small_jean.id]
+			end
+		end
+
+		describe "price_search" do
+			before do
+				@cheap_garment = create(:garment, price: 100)
+				@expensive_garment = create(:garment, price: 200)
+				@cheap_jean = create(:item, garment: @cheap_garment)
+				@expensive_jean = create(:item, garment: @expensive_garment)
+			end
+
+			it "should only return locations with microposts" do
+				get :home, { :min_price => 75, :max_price => 150 }
+				assigns(:jeans).map(&:id).should == [@cheap_jean.id]
+			end
+		end
+
+		describe "fit_search" do
+			before do
+				@skinny_garment = 		create(:garment, fit: "skinny")
+				@slim_straight_garment =  create(:garment, fit: "slim straight")
+				@skinny_jean = create(:item, garment: @skinny_garment)
+				@slim_straight_jean = create(:item, garment: @slim_straight_garment)
+			end
+
+			it "should only return locations with microposts" do
+				get :home, { :fit => "skinny" }
+				assigns(:jeans).map(&:id).should == [@skinny_jean.id]
+			end
+		end
+	end
+end
